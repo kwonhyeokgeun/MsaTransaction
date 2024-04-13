cdac817b0498f2ad1f8367240df567d8b4c589a4
kwonhyeokgeun <xkhg0611x@naver.com>
2024-04-13 23:56:15 +0900

    yml 수정

--- paypoint/src/main/java/com/example/paypoint/PaypointApplication.java
+++ paypoint/src/main/java/com/example/paypoint/PaypointApplication.java
@@ -3,11 +3,11 @@ package com.example.paypoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+//@EnableFeignClients
 public class PaypointApplication {
 
 	public static void main(String[] args) {
